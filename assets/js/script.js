2f3bb91da4319c6ade6bb87f95af4c46ed97baf6
Eli Pagan <mbk8689@gmail.com>
2021-10-28 04:59:49 -0400

    added quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var qustions = [
+    {
+        question: "1. Inside the HTML document, where do you place your JavaScript code?",
+        choices: ["Inside the <head> element", "In the <footer> element", "Inside the <link> element, Insie the <script> element"],
+        correctAnswer: 3
+    }, {
+        question: "2. How do we declare a conditional statment in JavaScript?",
+        choices: ["for loop", "if...else", "difference...between", "while loop"],
+        correctAnswer: 1
+    }, {
+        question: "3. When using flexbox, which property needs to be adjusted in order to add space between items?",
+        choices: ["justify-content", "flex-flow", "align-content", "space-between"],
+        correctAnswer: 0
+    }, {
+        question: "4. What are the two types of scope JavaScript uses?",
+        choices: ["Global and Local", "Surrounding and Inner", "Abroad and Local", "Outside and Inside"],
+        correctAnswer: 0
+    }, {
+        question: "5. How would you create a box with rounded corners using CSS?",
+        choices: ["border-radius: 50px;", "transform: round(corner)", "corner-style: round;", "box-corner: round;"],
+        correctAnswer : 0
+    }];
